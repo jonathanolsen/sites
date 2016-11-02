29bb856bb15031eae9833c06120457915cd078d7
jonathanolsen <cerity.se@gmail.com>
2016-11-02 11:28:29 +0100

    update

--- sense/assets/js/app.js
+++ sense/assets/js/app.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
 	var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
 
 	// build scene and set duration to window height
-	new ScrollMagic.Scene({triggerElement: ".Index-page-content"})
-		.setTween(".Index-page-content", {y: "80%", ease: Linear.easeNone})
+	new ScrollMagic.Scene({triggerElement: ".block-container.collection .block-content"})
+		.setTween(".block-container.collection .block-content", {y: "80%", ease: Linear.easeNone})
 		.addIndicators()
 		.addTo(controller);
 
