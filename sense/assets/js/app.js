48cfe036cdf84e5cd4f6fdd55b209c68231158d3
jonathanolsen <cerity.se@gmail.com>
2016-11-02 11:25:26 +0100

    update

--- sense/assets/js/app.js
+++ sense/assets/js/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 	$(window).scroll(function() {
-		var scroll = $(window).scrollTop();
-		$(".block-content").css("transform","translateY(" +  (scroll/2)  + "px)");
+
 	});
 });
