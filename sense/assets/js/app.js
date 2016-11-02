570eb8fb3298bdbeaab9b6c290470c69145ecbc6
jonathanolsen <cerity.se@gmail.com>
2016-11-02 13:30:04 +0100

    update

--- sense/assets/js/app.js
+++ sense/assets/js/app.js
@@ -1,30 +1,31 @@
 $(document).ready(function() {
-  if ($(".block-container").length) {
-    parallax();
-  }
+	function parallax(){
+		return $(this).each(function(){
+			var plxBackground = $(this).children('.block-content');
+			var plxWindow = $(this);
+
+			var plxWindowTopToPageTop = $(plxWindow).offset().top;
+			var windowTopToPageTop = $(window).scrollTop();
+			var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+
+			var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
+			var windowInnerHeight = window.innerHeight;
+			var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
+			var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
+			var plxSpeed = 0.35;
+
+			plxBackground.css("transform","translateY(" +  (plxWindowTopToWindowTop * plxSpeed) + 'px');
+		});
+	}
+
+	$(".block-container").parallax();
+
+	$(window).scroll(function(e) {
+		$(".block-container").parallax();
+	});
 });
 
-$(window).scroll(function(e) {
-  if ($(".block-container").length) {
-    parallax();
-  }
-});
 
-function parallax(){
-  if( $(".block-container").length > 0 ) {
-    var plxBackground = $(".block-content");
-    var plxWindow = $(".block-container");
 
-    var plxWindowTopToPageTop = $(plxWindow).offset().top;
-    var windowTopToPageTop = $(window).scrollTop();
-    var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
 
-    var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
-    var windowInnerHeight = window.innerHeight;
-    var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
-    var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
-    var plxSpeed = 0.15;
 
-    plxBackground.css("transform","translateY(" +  (plxWindowTopToWindowTop * plxSpeed) + 'px');
-  }
-}
