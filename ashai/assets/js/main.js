8d2a9d792d8d2009f87347185d869e88d6c7747f
jonathanolsen <cerity.se@gmail.com>
2017-03-03 22:12:31 +0100

    update

--- ashai/assets/js/main.js
+++ ashai/assets/js/main.js
@@ -3,18 +3,18 @@
 
 $(document).ready(function() {
 
-	$('.sqs-slide').addClass('.sqs-slide-animate');
+	$('.sqs-slide').addClass('sqs-slide-animate');
 
 	setTimeout(function() {
-		$('.sqs-slide > .sqs-slide-layer.full-width-height').addClass('.animate');
+		$('.sqs-slide > .sqs-slide-layer.full-width-height').addClass('animate');
 	}, 1000);
 
 	setTimeout(function() {
-		$('.sqs-slide .sqs-slide-layer-content').addClass('.animate');
+		$('.sqs-slide .sqs-slide-layer-content').addClass('animate');
 	}, 1400);
 
 	setTimeout(function() {
-		$('.sqs-slide').removeClass('.sqs-slide-animate');
+		$('.sqs-slide').removeClass('sqs-slide-animate');
 	}, 2000);
 
 });
