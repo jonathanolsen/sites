ed33f4d5cdb49f252c5fdcec72be43f6aa189215
jonathanolsen <cerity.se@gmail.com>
2017-03-04 00:26:05 +0100

    update

--- ashai/assets/js/main.js
+++ ashai/assets/js/main.js
@@ -11,22 +11,22 @@ $(document).ready(function() {
 
 	setTimeout(function() {
 		$('.sqs-slide .sqs-slide-layer.full-width-height').addClass('animate');
-	}, 3000);
+	}, 3600);
 
 	setTimeout(function() {
 		$('.sqs-slide .sqs-slide-layer-content.content-gallery').addClass('animate');
-	}, 4000);
+	}, 5200);
 
 	setTimeout(function() {
 		$('.sqs-slide .sqs-slide-layer-content.content-copy').addClass('animate');
-	}, 5000);
+	}, 6800);
 
 	setTimeout(function() {
 		$('.sqs-slide').removeClass('sqs-slide-animate');
 		$('.sqs-slide .sqs-slide-layer.full-width-height').removeClass('animate');
 		$('.sqs-slide .sqs-slide-layer-content.content-gallery').removeClass('animate');
 		$('.sqs-slide .sqs-slide-layer-content.content-copy').removeClass('animate');
-	}, 6000);
+	}, 7000);
 
 });
 
