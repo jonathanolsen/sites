c5e20c1d904ab58c5019975258bebc5c3bdd503d
jonathanolsen <cerity.se@gmail.com>
2017-03-03 22:32:56 +0100

    update

--- ashai/assets/js/main.js
+++ ashai/assets/js/main.js
@@ -7,25 +7,27 @@ $(document).ready(function() {
 
 	setTimeout(function() {
 		$('.sqs-slide').addClass('animate');
-	}, 1000);
-
-	setTimeout(function() {
-		$('.sqs-slide .sqs-slide-layer.full-width-height').addClass('animate');
 	}, 2000);
 
 	setTimeout(function() {
-		$('.sqs-slide .sqs-slide-layer-content.content-gallery').addClass('animate');
+		$('.sqs-slide .sqs-slide-layer.full-width-height').addClass('animate');
 	}, 3000);
 
 	setTimeout(function() {
-		$('.sqs-slide .sqs-slide-layer-content.content-copy').addClass('animate');
+		$('.sqs-slide .sqs-slide-layer-content.content-gallery').addClass('animate');
 	}, 4000);
+
+	setTimeout(function() {
+		$('.sqs-slide .sqs-slide-layer-content.content-copy').addClass('animate');
+	}, 5000);
 
 	/* setTimeout(function() {
 		$('.sqs-slide').removeClass('sqs-slide-animate');
 	}, 4000); */
 
 });
+
+
 
 
 
