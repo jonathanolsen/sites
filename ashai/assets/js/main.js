fd406db74c9aa2abc0fc6a0885a72ae7542a8f6e
jonathanolsen <cerity.se@gmail.com>
2017-03-04 23:28:27 +0100

    update

--- ashai/assets/js/main.js
+++ ashai/assets/js/main.js
@@ -1,19 +1,5 @@
 
-
-
-$(document).ready(function() {
-
-	$('body').prepend('<div class="our-story" id="our-story"><div class="container"><h2>The Story of Ashai</h2><p>Upon a visit to Kyoto in Japan, our founder met an old monk in an ancient temple. Nearly obsessed of his bead pouch filled with stones, we felt inspired to hear his story. The old man told us that his greatest wish was to find the twelve mythical volcanoes of Japan. In his search to find them, he collected lava stones from each and every volcano that crossed his path.</p><p>Back from his trip, our founders felt passionate about helping this old man fulfill his dream. The story about Ashai is to complete his lifelong journey, by releasing one collection dedicated to each mythical volcano, every year for the coming eight years.</p><a href="#" class="close-box">Close window</a></div></div>');
-	$('.sqs-slice-group.group-copy').append('<div class="price-container"><div class="price"><span>Official price</span>$79-99</div><div class="price"><span>Your discount</span>20%</div></div><div class="flickity gallery"><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/twelve.jpg" alt="" /></div><h4>The <span>Ashai Twelve</span></h4></div><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/kibun.jpg" alt="" /></div><h4><span>Kibun</span> Family</h4></div><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/nakama.jpg" alt="" /></div><h4><span>Nakama</span> Collection</h4></div></div>');
-
-	$('[href="#our-story"]').on('click', function() {
-		$('.our-story').addClass('active');
-	});
-
-	$('.close-box').on('click', function() {
-		$('.our-story').removeClass('active');
-	});
-
+function animation() {
 	var intervalTimer = null;
 	
 	intervalTimer = setInterval(function(){
@@ -56,10 +42,33 @@ $(document).ready(function() {
 		});
 		
 	}, 4800);
+}
+
+
+
+$(document).ready(function() {
+
+	$('body').prepend('<div class="our-story" id="our-story"><div class="container"><h2>The Story of Ashai</h2><p>Upon a visit to Kyoto in Japan, our founder met an old monk in an ancient temple. Nearly obsessed of his bead pouch filled with stones, we felt inspired to hear his story. The old man told us that his greatest wish was to find the twelve mythical volcanoes of Japan. In his search to find them, he collected lava stones from each and every volcano that crossed his path.</p><p>Back from his trip, our founders felt passionate about helping this old man fulfill his dream. The story about Ashai is to complete his lifelong journey, by releasing one collection dedicated to each mythical volcano, every year for the coming eight years.</p><a href="#" class="close-box">Close window</a></div></div>');
+	$('.sqs-slice-group.group-copy').append('<div class="price-container"><div class="price"><span>Official price</span>$79-99</div><div class="price"><span>Your discount</span>20%</div></div><div class="flickity gallery"><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/twelve.jpg" alt="" /></div><h4>The <span>Ashai Twelve</span></h4></div><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/kibun.jpg" alt="" /></div><h4><span>Kibun</span> Family</h4></div><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/nakama.jpg" alt="" /></div><h4><span>Nakama</span> Collection</h4></div></div>');
+
+	$('[href="#our-story"]').on('click', function() {
+		$('.our-story').addClass('active');
+	});
+
+	$('.close-box').on('click', function() {
+		$('.our-story').removeClass('active');
+	});
+
+	if($(window).width() >= 768){
+		animation();
+	}
+	
 
 	
 
 });
+
+
 
 
 
