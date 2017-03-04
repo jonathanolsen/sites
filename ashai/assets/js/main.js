df450861d65e33cca1fa7da3cf18c5c8aaedfe83
jonathanolsen <cerity.se@gmail.com>
2017-03-04 21:44:07 +0100

    update

--- ashai/assets/js/main.js
+++ ashai/assets/js/main.js
@@ -4,7 +4,7 @@
 $(document).ready(function() {
 
 	$('body').prepend('<div class="our-story" id="our-story"><div class="container"><h2>The Story of Ashai</h2><p>Upon a visit to Kyoto in Japan, our founder met an old monk in an ancient temple. Nearly obsessed of his bead pouch filled with stones, we felt inspired to hear his story. The old man told us that his greatest wish was to find the twelve mythical volcanoes of Japan. In his search to find them, he collected lava stones from each and every volcano that crossed his path.</p><p>Back from his trip, our founders felt passionate about helping this old man fulfill his dream. The story about Ashai is to complete his lifelong journey, by releasing one collection dedicated to each mythical volcano, every year for the coming eight years.</p><a href="#" class="close-box">Close window</a></div></div>');
-	$('.sqs-slice-group.group-copy').append('<div class="price-container"><div class="price"><span>Official price</span>$79-99</div><div class="price"><span>Your discount</span>20%</div></div><div class="bracelets"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/bracelets.jpg" /></div><div class="flickity gallery"><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/twelve-one.jpg" alt="" /></div><h4>The <span>Ashai Twelve</span></h4></div><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/kibun-gold.jpg" alt="" /></div><h4><span>Kibun</span> Family</h4></div><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/kibun-black.jpg" alt="" /></div><h4><span>Kibun</span> Family</h4></div><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/nakama-gold.jpg" alt="" /></div><h4><span>Nakama</span> Collection</h4></div><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/nakama-silver.jpg" alt="" /></div><h4><span>Nakama</span> Collection</h4></div><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/nakama-rose.jpg" alt="" /></div><h4><span>Nakama</span> Collection</h4></div></div>');
+	$('.sqs-slice-group.group-copy').append('<div class="price-container"><div class="price"><span>Official price</span>$79-99</div><div class="price"><span>Your discount</span>20%</div></div><div class="bracelets"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/bracelets.jpg" /></div><div class="flickity gallery"><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/twelve.jpg" alt="" /></div><h4>The <span>Ashai Twelve</span></h4></div><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/kibun.jpg" alt="" /></div><h4><span>Kibun</span> Family</h4></div><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/nakama.jpg" alt="" /></div><h4><span>Nakama</span> Collection</h4></div></div>');
 
 	$('[href="#our-story"]').on('click', function() {
 		$('.our-story').addClass('active');
@@ -60,6 +60,8 @@ $(document).ready(function() {
 	
 
 });
+
+
 
 
 
