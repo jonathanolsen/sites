6e5e6f87613c714ade20aa75305f16ffb8b7dc93
jonathanolsen <cerity.se@gmail.com>
2017-03-07 22:00:41 +0100

    update

--- ashai/assets/js/main3.js
+++ ashai/assets/js/main3.js
@@ -3,14 +3,13 @@
 
 $(document).ready(function() {
 
-	
-	$('.sqs-slice-group.group-copy').append('<div><div class="price-container"><div class="price" style="color: #EC5466; font-weight: 600;"><span>Your discount</span>20%</div><div class="price"><span>Official price</span>$79-99</div></div></div><div class="flickity gallery"><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/twelve.jpg" alt="" /></div><h4>The <span>Ashai Twelve</span></h4></div><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/kibun.jpg" alt="" /></div><h4><span>Kibun</span> Family</h4></div><div class="gallery-item"><div class="image"><img src="https://jonathanolsen.github.io/sites/ashai/assets/img/nakama.jpg" alt="" /></div><h4><span>Nakama</span> Collection</h4></div></div><a href="#our-story" class="story-btn">The Story of Ashai</a>');
-
 
 
 	$('.sqs-slice-group.group-copy').append('<div class="limited-count"><span>Exclusive early bird</span><div class="count">387/500</div></div>');
 
 });
+
+
 
 
 
