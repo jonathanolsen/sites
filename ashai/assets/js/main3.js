7b704b6c6b5a0b21f4df4afa65395d38d62a8556
jonathanolsen <cerity.se@gmail.com>
2017-03-07 22:33:02 +0100

    update

--- ashai/assets/js/main3.js
+++ ashai/assets/js/main3.js
@@ -5,9 +5,11 @@ $(document).ready(function() {
 
 
 
-	$('.sqs-slice-group.group-copy .sqs-slice-body-content').append('<div class="limited-count"><span>Exclusive early bird</span><div class="count">387/500</div></div>');
+	$('.sqs-slice-group.group-copy .sqs-slice-body-content').append('<div class="limited-count"><span>35% early bird discount</span><div class="count">387/500</div></div>');
 
 });
+
+
 
 
 
