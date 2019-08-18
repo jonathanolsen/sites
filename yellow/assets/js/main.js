818220bf986b010f8ebcb2cefb0f93c751914908
Jonathan Olsen <mail@lilypad.se>
2019-08-18 17:36:53 +0200

    update

--- yellow/assets/js/main.js
+++ yellow/assets/js/main.js
@@ -1,1 +1,57 @@
-$.fn.parallax=function(){return $(this).each(function(){var n=$(this).children(".block-content"),t=$(this),o=$(t).offset().top,c=$(window).scrollTop(),i=o-c,a=$(n).offset().top;window.innerHeight;n.css("transform","translateY("+(.08*i-120)+"px")})},$(document).ready(function(){$(".block-container").parallax(),$(window).scroll(function(n){$(".block-container").parallax()}),$(".flickity").flickity({watchCSS:!0})});
+// When scrolling down, hide header. Scrolling up, show header. Fixed.
+var w = $(window);
+var viewTop = w.scrollTop();
+var up = false;
+var newscroll;
+var sensitivity = 2;
+
+w.on("scroll", function() {
+if ($(this).scrollTop() > 140) {
+
+  $("body").addClass("is-scrolled");
+
+  if ($(this).scrollTop() > 600) {
+    newscroll = w.scrollTop();
+
+    if (newscroll > sensitivity + viewTop && !up) {
+      $("body").addClass("menu-hidden");
+      up = !up;
+    } else if (newscroll + sensitivity < viewTop && up) {
+      $("body").removeClass("menu-hidden");
+      up = !up;
+    }
+  } else {
+  }
+
+  viewTop = newscroll;
+} else {
+  $("body").removeClass("is-scrolled menu-hidden");
+}
+});
+
+
+function scrollFX() {
+  var $window = $(window),
+    scrollTop = $window.scrollTop(),
+    $boxes = $('[data-scroll-speed]');
+
+  $boxes.each(function() {
+    var $this = $(this),
+      oTop = scrollTop - $this.offset().top,
+      scrollspeed = parseInt($this.data('scroll-speed')),
+      val = -oTop / scrollspeed;
+    $this.css('transform', 'translateY(' + val + 'px)');
+  });
+}
+
+$(window).scroll(function() {
+  scrollFX();
+});
+
+$(window).resize(function() {
+  scrollFX();
+});
+
+$(document).ready(function() {
+  scrollFX();
+});
