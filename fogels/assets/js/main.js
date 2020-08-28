a99020923cc5b7a9e7efaabf54a8d2c6a3d6b6bb
Jonathan Olsen <mail@lilypad.se>
2020-08-28 11:31:32 +0200

    update

--- fogels/assets/js/main.js
+++ fogels/assets/js/main.js
@@ -5,53 +5,87 @@ var up = false;
 var newscroll;
 var sensitivity = 2;
 
-w.on("scroll", function() {
-if ($(this).scrollTop() > 140) {
+w.on("scroll", function () {
+  if ($(this).scrollTop() > 140) {
+    $("body").addClass("is-scrolled");
 
-  $("body").addClass("is-scrolled");
+    if ($(this).scrollTop() > 600) {
+      newscroll = w.scrollTop();
 
-  if ($(this).scrollTop() > 600) {
-    newscroll = w.scrollTop();
-
-    if (newscroll > sensitivity + viewTop && !up) {
-      $("body").addClass("menu-hidden");
-      up = !up;
-    } else if (newscroll + sensitivity < viewTop && up) {
-      $("body").removeClass("menu-hidden");
-      up = !up;
+      if (newscroll > sensitivity + viewTop && !up) {
+        $("body").addClass("menu-hidden");
+        up = !up;
+      } else if (newscroll + sensitivity < viewTop && up) {
+        $("body").removeClass("menu-hidden");
+        up = !up;
+      }
+    } else {
     }
+
+    viewTop = newscroll;
   } else {
+    $("body").removeClass("is-scrolled menu-hidden");
   }
-
-  viewTop = newscroll;
-} else {
-  $("body").removeClass("is-scrolled menu-hidden");
-}
 });
-
 
 function scrollFX() {
   var $window = $(window),
     scrollTop = $window.scrollTop(),
-    $boxes = $('[data-scroll-speed]');
+    $boxes = $("[data-scroll-speed]");
 
-  $boxes.each(function() {
+  $boxes.each(function () {
     var $this = $(this),
       oTop = scrollTop - $this.offset().top,
-      scrollspeed = parseInt($this.data('scroll-speed')),
+      scrollspeed = parseInt($this.data("scroll-speed")),
       val = -oTop / scrollspeed;
-    $this.css('transform', 'translateY(' + val + 'px)');
+    $this.css("transform", "translateY(" + val + "px)");
   });
 }
 
-$(window).scroll(function() {
+$(window).scroll(function () {
   scrollFX();
 });
 
-$(window).resize(function() {
+$(window).resize(function () {
   scrollFX();
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
   scrollFX();
+});
+
+$(document).on("mousedown", "[data-ripple], .rippledown", function (e) {
+  var $self = $(this);
+  if ($self.is(".btn-disabled")) {
+    return;
+  }
+  if ($self.closest("[data-ripple]")) {
+    e.stopPropagation();
+  }
+  var initPos = $self.css("position"),
+    offs = $self.offset(),
+    x = e.pageX - offs.left,
+    y = e.pageY - offs.top,
+    dia = Math.min(this.offsetHeight, this.offsetWidth, 100), // start diameter
+    $ripple = $("<div/>", { class: "ripple", appendTo: $self });
+
+  if (!initPos || initPos === "static") {
+    $self.css({ position: "relative" });
+  }
+  $("<div/>", {
+    class: "rippleWave",
+    css: {
+      background: $self.data("ripple"),
+      width: dia,
+      height: dia,
+      left: x - dia / 2,
+      top: y - dia / 2,
+    },
+    appendTo: $ripple,
+    one: {
+      animationend: function () {
+        $ripple.remove();
+      },
+    },
+  });
 });
